8eb4589bfe0ec86bde8bfd456cec9c6a0b76ec96
daniel01silva <52723069+daniel01silva@users.noreply.github.com>
2019-07-10 20:23:49 -0300

    ex01 pega final da palavra

---
+++ fase02/ex01/finais.js
@@ -0,0 +1,9 @@
+function finais(palavra) {
+    var final;
+    final = palavra.charAt(palavra.length - 3);
+    final += palavra.charAt(palavra.length - 2);
+    final += palavra.charAt(palavra.length - 1);
+    
+    return final
+
+}
