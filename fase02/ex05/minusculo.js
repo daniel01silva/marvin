c35c1c5c6ad0bb017ecbc86810dd5584209da884
daniel01silva <52723069+daniel01silva@users.noreply.github.com>
2019-07-10 21:25:21 -0300

    correçao

--- fase02/ex05/minusculo.js
+++ fase02/ex05/minusculo.js
@@ -1,7 +1,3 @@
 function minusculo(palavra) {
-    
-    var inicio = palavra.substr(0,1).toLowerCase();
-    var final = palavra.substr(1, palavra.length);
-    
-    return inicio + final
+    return palavra.substr(0,1).toLowerCase();
 }
